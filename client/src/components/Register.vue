38a3c7472a410d19028d19e3269d43fd4b40c7ce
Michael <sheveksmath@gmail.com>
2019-03-08 15:18:17 -0500

    implented vuetify on client-side, now shows validation errors

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,30 +1,45 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="email" v-model="email">
-    <input type="password" name="password" placeholder="password" v-model="password">
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2"></div>
+      <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input type="email" name="email" placeholder="email" v-model="email">
+        <br>
+        <input type="password" name="password" placeholder="password" v-model="password">
+        <br>
+        <div class="error" v-html="error"/>
+        <br>
+        <v-btn class="cyan" @click="register">Register</v-btn>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+/* eslint-disable */
 import AuthenticationService from "@/services/AuthenticationService";
 export default {
   name: "HelloWorld",
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      error: null
     };
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      });
-      console.log(response.data);
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
     }
   }
 };
@@ -32,4 +47,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 </style>
