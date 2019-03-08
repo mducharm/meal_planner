bb0d4c99bdf3544c48efe9abe0ca7cf30939f3c7
Michael <sheveksmath@gmail.com>
2019-03-07 22:45:45 -0500

    set up controller folder, connected controllers to routes.js

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const config = require("./config/config");
 
 const app = express();
 app.use(morgan("combined"));
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,5 @@
+const AuthenticationController = require("./controllers/AuthenticationController");
+
 module.exports = app => {
-  //   app.get("/status", (req, res) => {
-  //     res.send({
-  //       message: "Hello"
-  //     });
-  //   }),
-  app.post("/register", (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}`
-    });
-  });
+  app.post("/register", AuthenticationController.register);
 };
